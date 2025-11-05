c4935a1a8d0077862a398fce4b1932a5e5a34648
zakaria allam <zakariaallam536@gmail.com>
2025-11-05 12:06:29 +0100

    creation de Add list et modification de header

---
+++ main.js
@@ -0,0 +1,76 @@
+// ------- DOM Freelances --------------------
+const btn_freelance = document.getElementById("btn_freelance");
+const Carte = document.getElementById("Carte"); 
+const img_freelance = document.getElementById("img_freelance")
+const name_freelance = document.getElementById("name_freelance")
+const skils_freeLance = document.getElementById("skils_freeLance")
+const specialisee = document.getElementById("specialisee")
+const amuont_freeLance = document.getElementById("amuont_freeLance")
+const div_row = document.getElementById("div_row")
+const header = document.getElementById("header")
+
+let ListFreeLence ;
+
+// ---------- Data Json ----------------
+
+
+async function fetshdata(file){
+    let get_data = await fetch(file);
+    let xml = await get_data.text();
+    ListFreeLence = JSON.parse(xml);
+
+    AddListFreeLance();
+}
+
+fetshdata('/services/data.json');
+
+// ------------- header ----------------
+async function fetsh_header(file){
+    let get_data = await fetch(file);
+    let xml = await get_data.text();
+    header.innerHTML = xml;
+
+}
+
+fetsh_header('/components/header.html');
+
+// --------------- Card -------------------
+
+
+
+
+// ---------functions ----------------
+
+
+function AddListFreeLance(){
+
+    ListFreeLence.forEach(element => {
+
+         let card = `<div class="col-md-4">
+        <div id = "Carte" class="card h-100 shadow-sm">
+          <div class="card-body d-flex gap-3">
+            <img id="img_freelance" src=${element.photos} alt="Mouad" class="card-avatar">
+            <div>
+              <h5 id="name_freelance"  class="card-title mb-1">${element.fullName}</h5>
+              <small id="skils_freeLance" class="text-muted">${element.skils}</small>
+              <p id="specialisee" class="mt-2 mb-1 text-muted">${element.spécialisations}</p>
+              <div class="text-warning">
+                ★★★★☆ <small class="text-muted">(4.7)</small>
+              </div>
+            </div>
+          </div>
+          <div class="card-footer bg-transparent d-flex justify-content-between">
+            <small id="amuont_freeLance" class="text-muted">${element.amaunt}</small>
+            <button class="btn btn-sm btn-outline-primary" data-bs-toggle="modal" data-bs-target="#profilModal">Voir</button>
+          </div>
+        </div>
+      </div>` 
+
+      div_row.innerHTML += card
+        
+    });
+
+}
+
+// ---------------- filter -----------------------
+
